e60156a6d1bc41b4c6b195a4bb67a0fac0a88595
Akiranishimura <nissi20ch@gmail.com>
2023-11-24 07:19:17 +0900

    Fix date calculation in index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -196,8 +196,8 @@ console.log(isMatched); // 結果: trueまたはfalse
       let today = new Date();
       let todayYear = today.getFullYear();
       let todayMonth = today.getMonth() + 1;
-      let todayDate = today.getDate() -1 ;
-      let todayDatePlus1 = todayDate +1;
+      let todayDate = today.getDate() ;
+      let todayDatePlus1 = todayDate;
       // let todayMonth = 11
       let todayString = todayYear + '-' + todayMonth + '-' + todayDate + 'T00:00:00.000Z'; //なぜかT15:00:00.000Z設定になっているのでこれにする
       let todayStringPlus1 = todayYear + '-' + todayMonth + '-' + todayDatePlus1 + 'T00:00:00.000Z'; //なぜかT15:00:00.000Z設定になっているのでこれにする
